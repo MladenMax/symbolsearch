cf45eb1c4de25024cde1c23b82daa86e8ca9e258
Mladen Maksimovic <mladenmaksimovic@pop-os.localdomain>
2019-03-15 17:20:23 +0100

    refactored TabNavigator

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,54 +1,78 @@
-import React from "react";
-import { Platform } from "react-native";
+import React from 'react';
+import { Platform, Text } from 'react-native';
 import {
-  createStackNavigator,
-  createBottomTabNavigator
-} from "react-navigation";
+	createStackNavigator,
+	createBottomTabNavigator,
+} from 'react-navigation';
 
-import TabBarLabel from "../components/TabBarLabel";
-import TabBarIcon from "../components/TabBarIcon";
-import SearchScreen from "../screens/SearchScreen";
-import FavoritesScreen from "../screens/FavoritesScreen";
+import SearchScreen from '../screens/SearchScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+
+import Icon from '../components/Icon';
+import { icons } from '../consts/icons';
+
+import { styles } from '../styles/TabNavigator';
+import { colors } from '../consts/colors';
 
 const SearchStack = createStackNavigator(
-  {
-    Search: SearchScreen
-  },
-  { header: null, headerMode: "none" }
+	{
+		Search: SearchScreen,
+	},
+	{ header: null, headerMode: 'none' }
 );
 
 SearchStack.navigationOptions = {
-  tabBarLabel: ({ focused }) => (
-    <TabBarLabel label={"Market Search"} focused={focused} />
-  ),
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === "ios" ? `ios-list` : "md-list"}
-    />
-  )
+	tabBarLabel: ({ focused }) => {
+		const { label } = styles;
+		return (
+			<Text
+				style={[label, { color: focused ? colors.focused : colors.blured }]}
+			>
+				{'Market Search'}
+			</Text>
+		);
+	},
+	tabBarIcon: ({ focused }) => {
+		return (
+			<Icon
+				name={icons.list[Platform.OS]}
+				size={26}
+				color={focused ? colors.focused : colors.blured}
+			/>
+		);
+	},
 };
 
 const FavoritesStack = createStackNavigator(
-  {
-    Favorites: FavoritesScreen
-  },
-  { header: null, headerMode: "none" }
+	{
+		Favorites: FavoritesScreen,
+	},
+	{ header: null, headerMode: 'none' }
 );
 
 FavoritesStack.navigationOptions = {
-  tabBarLabel: ({ focused }) => (
-    <TabBarLabel label={"Favorites"} focused={focused} />
-  ),
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === "ios" ? `ios-heart` : "md-heart"}
-    />
-  )
+	tabBarLabel: ({ focused }) => {
+		const { label } = styles;
+		return (
+			<Text
+				style={[label, { color: focused ? colors.focused : colors.blured }]}
+			>
+				{'Favorites'}
+			</Text>
+		);
+	},
+	tabBarIcon: ({ focused }) => {
+		return (
+			<Icon
+				name={icons.heart[Platform.OS]}
+				size={26}
+				color={focused ? colors.focused : colors.blured}
+			/>
+		);
+	},
 };
 
 export default createBottomTabNavigator({
-  SearchStack,
-  FavoritesStack
+	SearchStack,
+	FavoritesStack,
 });
---
+++ styles/TabNavigator.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	label: {
+		textAlign: 'center',
+		fontSize: 10,
+		paddingBottom: 5,
+	},
+});

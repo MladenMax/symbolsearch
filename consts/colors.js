46b2d2e2b0579140c6c81389610bc8f8532b53d6
Mladen Maksimovic <mladenmaksimovic@pop-os.localdomain>
2019-03-15 17:18:11 +0100

    added colors and icon names consts

---
+++ consts/colors.js
@@ -0,0 +1,8 @@
+import { theme } from '../styles/App';
+
+export const colors = {
+  focused: theme.colors.primary,
+  blured: theme.colors.disabled,
+  follow: 'red',
+  unfollow: '#999999',
+} 
---
+++ consts/icons.js
@@ -0,0 +1,14 @@
+export const icons = {
+	list: {
+		ios: 'ios-list',
+		android: 'md-list',
+	},
+	heart: {
+		ios: 'ios-heart',
+		android: 'md-heart',
+	},
+	heart_empty: {
+		ios: 'ios-heart-empty',
+		android: 'md-heart-empty',
+	},
+};

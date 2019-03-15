946b00a271e15631a8901a42e165b34384feb754
Mladen Maksimovic <mladenmaksimovic@pop-os.localdomain>
2019-03-15 17:19:38 +0100

    refactored DataRow component

--- components/DataRow.js
+++ components/DataRow.js
@@ -1,36 +1,45 @@
-import React, { Component } from "react";
-import { Text } from "react-native";
-import { DataTable } from "react-native-paper";
-import PropTypes from "prop-types";
+import React from 'react';
+import { Platform, Text } from 'react-native';
+import { DataTable } from 'react-native-paper';
+import PropTypes from 'prop-types';
 
-import DataRowIcon from "./DataRowIcon";
+import Icon from '../components/Icon';
+import { icons } from '../consts/icons';
+import { colors } from '../consts/colors';
 
-import { styles } from "../styles/DataRow";
+import { styles } from '../styles/DataRow';
 
-class DataRow extends Component {
-  static propTypes = {
-    name: PropTypes.string,
-    value: PropTypes.number,
-    following: PropTypes.bool,
-    followSymbol: PropTypes.func,
-    openSymbol: PropTypes.func
-  };
+const DataRow = ({ name, value, following, followSymbol, openSymbol }) => {
+	const { row, nameCell, valueCell, valueText, followingCell } = styles;
+	return (
+		<DataTable.Row style={row}>
+			<DataTable.Cell style={nameCell} onPress={openSymbol}>
+				{name}
+			</DataTable.Cell>
+			<DataTable.Cell style={valueCell}>
+				<Text style={valueText}>{`$ ${value.toFixed(2)}`}</Text>
+			</DataTable.Cell>
+			<DataTable.Cell style={followingCell} onPress={followSymbol}>
+				<Icon
+					name={
+						following
+							? icons.heart[Platform.OS]
+							: icons.heart_empty[Platform.OS]
+					}
+					size={25}
+					color={following ? colors.follow : colors.unfollow}
+				/>
+			</DataTable.Cell>
+		</DataTable.Row>
+	);
+};
 
-  render() {
-    const { name, value, following, followSymbol, openSymbol } = this.props;
-    const { row, nameCell, valueCell, valueText, followingCell } = styles;
-    return (
-      <DataTable.Row style={row}>
-        <DataTable.Cell style={nameCell} onPress={openSymbol}>{name}</DataTable.Cell>
-        <DataTable.Cell style={valueCell}>
-          <Text style={valueText}>{`$ ${value.toFixed(2)}`}</Text>
-        </DataTable.Cell>
-        <DataTable.Cell style={followingCell} onPress={followSymbol}>
-          <DataRowIcon following={following} />
-        </DataTable.Cell>
-      </DataTable.Row>
-    );
-  }
-}
+DataRow.propTypes = {
+	name: PropTypes.string,
+	value: PropTypes.number,
+	following: PropTypes.bool,
+	followSymbol: PropTypes.func,
+	openSymbol: PropTypes.func,
+};
 
 export default DataRow;
